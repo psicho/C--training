51f8dc5d3356e336a103be5c55b51dc7c983614a
psicho <9400878@gmail.com>
2018-01-03 19:25:41 +0400

    Рассчет начисленных процентов по сумме вклада, доходности и сроку.

---
+++ 001/ClientInput/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var userInput = Console.ReadLine();
+            Console.WriteLine(Calculate(userInput));
+            Console.WriteLine("Нажмите любую клавишу");
+            Console.ReadLine();
+        }
+
+        public static double Calculate(string userInput)
+        {
+            var solver = userInput.Split(' ');
+            Console.WriteLine(solver[0]);
+
+            var percent = Convert.ToDouble(solver[1]) / 12 / 100 + 1;
+            var summ = Convert.ToDouble(solver[0]);
+
+            for (var i=1; i <= Convert.ToInt32(solver[2]); i++)
+            {
+                summ *= percent;
+            }
+            return summ;
+        }
+    }
+}
